b57fa7f20f5a5e562cadbaeed96efc59574bfde3
liulingfeng <854034241@qq.com>
2016-07-25 17:52:21 +0800

    third commit

--- app/src/main/java/com/llf/lib/design/AppBarLayoutActivity.java
+++ app/src/main/java/com/llf/lib/design/AppBarLayoutActivity.java
@@ -3,15 +3,23 @@ package com.llf.lib.design;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import com.llf.lib.R;
 
 /**
  * Created by llf on 2016/7/25.
  */
 public class AppBarLayoutActivity extends AppCompatActivity{
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appbarlayout);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("美眉");
+        toolbar.setNavigationIcon(R.drawable.back);
+        setSupportActionBar(toolbar);
     }
 }
