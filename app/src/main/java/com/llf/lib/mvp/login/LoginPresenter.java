eb0d8871032203ba2025aca40ba51bb9f60f033f
liulingfeng <854034241@qq.com>
2016-08-01 18:30:31 +0800

    MVP

--- app/src/main/java/com/llf/lib/mvp/login/LoginPresenter.java
+++ app/src/main/java/com/llf/lib/mvp/login/LoginPresenter.java
@@ -1,6 +1,9 @@
 package com.llf.lib.mvp.login;
 
 import android.text.TextUtils;
+import com.llf.lib.net.OkHttpUtil;
+import com.squareup.okhttp.Request;
+import java.io.IOException;
 
 /**
  * Created by llf on 2016/8/1.
@@ -31,7 +34,20 @@ public class LoginPresenter implements LoginContract.Presenter{
     @Override
     public void toLogin(String userName,String passWord) {
         if(check(userName,passWord)){
-            view.showResult("登录成功");
+            view.showLoading();
+            OkHttpUtil.getInstanse().cook("http://apis.juhe.cn/cook/query.php", "秘制红烧肉", "273c2928ac2cf691d8f6d3372da48b23", new OkHttpUtil.StringCallback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    view.hideLoading();
+                }
+
+                @Override
+                public void onResponse(String response) {
+                    view.hideLoading();
+                    view.showResult(response);
+                }
+            });
+
         }
     }
 
