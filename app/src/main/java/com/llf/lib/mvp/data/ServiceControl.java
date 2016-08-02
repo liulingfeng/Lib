db64aee475205bb78000534304e8098738f15658
liulingfeng <854034241@qq.com>
2016-08-02 10:39:09 +0800

    MVP

---
+++ app/src/main/java/com/llf/lib/mvp/data/ServiceControl.java
@@ -0,0 +1,17 @@
+package com.llf.lib.mvp.data;
+
+import com.llf.lib.net.OkHttpUtil;
+import com.squareup.okhttp.FormEncodingBuilder;
+
+/**
+ * Created by llf on 2016/8/2.
+ * 接口中心
+ */
+public class ServiceControl {
+    public static void cookQuery(String url, String menu, String key, OkHttpUtil.StringCallback callback){
+        FormEncodingBuilder builder = HttpHeader.initFormEncodingBuilder();
+        builder.add("menu",menu);
+        builder.add("key",key);
+        OkHttpUtil.getInstanse().postAsyn(url,builder,callback);
+    }
+}
