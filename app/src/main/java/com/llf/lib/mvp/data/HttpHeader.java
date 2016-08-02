f7bf3cf65177a5b0b6b948f9b5b5790264e9183c
liulingfeng <854034241@qq.com>
2016-08-02 10:39:17 +0800

    MVP

---
+++ app/src/main/java/com/llf/lib/mvp/data/HttpHeader.java
@@ -0,0 +1,20 @@
+package com.llf.lib.mvp.data;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+
+/**
+ * Created by llf on 2016/8/2.
+ * 公共的参数集成到这里
+ */
+public class HttpHeader {
+    static {
+        //这边获取公共的参数
+    }
+    protected static FormEncodingBuilder initFormEncodingBuilder(){
+        FormEncodingBuilder formEncodingBuilder = new FormEncodingBuilder();
+//        formEncodingBuilder.add("version", version);
+//        formEncodingBuilder.add("imsi", ""+imsi);
+//        formEncodingBuilder.add("imei", ""+imei);
+        return formEncodingBuilder;
+    }
+}
